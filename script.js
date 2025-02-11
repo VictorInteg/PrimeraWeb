3332f7e52bed0b3d08a39b74ff7928940d90b9c8
VictorInteg <47134178@continental.edu.pe>
2025-02-11 01:47:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensaje() {
+    alert("¡Hola! Has hecho clic en el botón.");
+
+    let boton = document.querySelector("button");
+    boton.style.animation = "colorChange 1s infinite alternate";
+}
